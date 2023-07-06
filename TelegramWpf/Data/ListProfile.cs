01b2552f2681fdf9a4b6a9079677230c34446bb3
MesterBrok <Brokdeveloper@gmail.com>
2023-07-06 14:27:13 +0330

    update Items

--- TelegramWpf/Data/ListProfile.cs
+++ TelegramWpf/Data/ListProfile.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using TelegramWpf.Models;
 
 namespace TelegramWpf.Data
 {
-    public  class ListProfile
+    public class ListProfile
     {
         public static ObservableCollection<Profile> Profiles
         {
@@ -13,18 +18,18 @@ namespace TelegramWpf.Data
                 {
                     new Profile
                     {
-                        ContactName = "John",
+                        ContactName = "Mohammad",
                         LastMessage = "Hello!",
                         DateTime = "2023-07-05 10:30:00",
-                        ImageProfile = "profile1.jpg",
-                        MessageCount= 5
+                        ImageProfile = "Resources/Images/Img2.jpg",
+                        MessageCount = 5
                     },
                     new Profile
                     {
-                        ContactName = "Emma",
+                        ContactName = "Hani",
                         LastMessage = "How are you?",
                         DateTime = "2023-07-04 15:45:00",
-                        ImageProfile = "profile2.jpg",
+                        ImageProfile = "Resources/Images/Img1.jpg",
                         MessageCount = 10
                     },
                     new Profile
@@ -32,15 +37,15 @@ namespace TelegramWpf.Data
                         ContactName = "Michael",
                         LastMessage = "Can we meet tomorrow?",
                         DateTime = "2023-07-03 18:20:00",
-                        ImageProfile = "profile3.jpg",
+                        ImageProfile = "Resources/Images/Img3.jpg",
                         MessageCount = 8
                     },
                     new Profile
                     {
-                        ContactName = "Sophia",
+                        ContactName = "Brok",
                         LastMessage = "Where are you?",
                         DateTime = "2023-07-02 20:10:00",
-                        ImageProfile = "profile4.jpg",
+                        ImageProfile = "Resources/Images/Img4.jpg",
                         MessageCount = 2
                     }
                 };
